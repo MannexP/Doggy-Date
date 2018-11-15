c7c53a5443905b9ab4f9a8dc3efb07a3f82ca660
Mannex Pimentel <Mannex@Mannexs-MacBook-Air.local>
2018-11-15 11:27:18 -0500

    components

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
+// import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+import HomePage from './components/HomePage'
+import DogPage from './components/DogPage'
+import DogsPage from './components/DogsPage'
+import DatePage from './components/DatePage'
+import NavBar from './components/NavBar'
+// import { createGlobalStyle } from 'styled-components'
 
 
 class App extends Component {
   render() {
     return (
-      <div>
-       
-      </div>
+      // <Router>
+        <div>
+          {/* <Global />
+          <NavBar />
+          <Switch>
+            <Route exact path="/dogs" component={DogPage}/>
+            <Route exact path="/dogs/:dogId" component={IdeaPage}/>
+            <Route path="/" component={HomePage}/>
+          </Switch> */}
+        </div>
+      // </Router>
     );
   }
 }
