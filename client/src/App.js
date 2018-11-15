d70fd59f7498b6fcbbab585559035218eeb375ec
Mannex Pimentel <Mannex@Mannexs-MacBook-Air.local>
2018-11-15 14:11:48 -0500

    starting from scratch

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,13 @@
-import React, { Component } from 'react'
-// eslint-disable-next-line
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
-// import { createGlobalStyle } from 'styled-components'
+import React, { Component } from 'react';
 
-// import HomePage from './components/HomePage'
-
-import DogsPage from './components/DogsPage'
-import DogPage from './components/DogPage'
-
-
-
-
-
-class App
- extends Component {
-    render() {
-        return (
-            <Router>
-            <div>
-            
-              <NavBar />
-              <Switch>
-                {/* <Route exact path="/login" component={LogInPage}/> */}
-                <Route exact path="/dogs/:dogId" component={DogPage}/>
-                {/* <Route path="/" component={HomePage}/> */}
-              </Switch>
-            </div>
-          </Router>
-        );
-    }
+class App extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
 }
 
-export default App
-;
+export default App;
