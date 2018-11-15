09f8d81c0ee92707c407ad929a46deb07701b156
Mannex Pimentel <Mannex@Mannexs-MacBook-Air.local>
2018-11-15 14:32:20 -0500

    re-component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+
+import HomePage from './components/HomePage'
+import Dogs from './components/Dogs'
+import Dog from'./components/Dog'
+import NavBar from './components/NavBar'
+
+
+
 
 class App extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+      <Router>
+        <div>
+          <Global />
+          <NavBar />
+          <Switch>
+            <Route exact path="/dogs" component={Dogs}/>
+            <Route exact path="/dogs/:dogId" component={Dog}/>
+            <Route path="/" component={HomePage}/>
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
