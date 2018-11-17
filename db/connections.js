5e748f1b5d024b6c0fc5d4003ec39ab5be8c725d
Mannex Pimentel <Mannex@Mannexs-MacBook-Air.local>
2018-11-16 21:49:59 -0500

    heroku test

--- db/connections.js
+++ db/connections.js
@@ -1,15 +1,23 @@
+// require('dotenv').config()
+// const mongoose = require('mongoose')
+
+// mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+
+// mongoose.connection.once('open', () => {
+//     console.log(`Mongoose has connected to MongoDB`)
+// })
+
+// mongoose.connection.on('error', (error) => {
+//     console.error(`MongoDB connection error!!! ${error}`)
+//     process.exit(-1)
+// })
+
+// module.exports = mongoose
 require('dotenv').config()
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
-
-mongoose.connection.once('open', () => {
-    console.log(`Mongoose has connected to MongoDB`)
-})
-
-mongoose.connection.on('error', (error) => {
-    console.error(`MongoDB connection error!!! ${error}`)
-    process.exit(-1)
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+    console.log('Connected to mongo');
 })
 
 module.exports = mongoose
