b84683e9c7467f0d9021ff76ea15a6f6f0018679
Mannex Pimentel <Mannex@Mannexs-MacBook-Air.local>
2018-11-20 10:21:35 -0500

    db connection test

--- db/connections.js
+++ db/connections.js
@@ -1,24 +1,24 @@
-// require('dotenv').config()
-// const mongoose = require('mongoose')
-
-// mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
-
-// mongoose.connection.once('open', () => {
-//     console.log(`Mongoose has connected to MongoDB`)
-// })
-
-// mongoose.connection.on('error', (error) => {
-//     console.error(`MongoDB connection error!!! ${error}`)
-//     process.exit(-1)
-// })
-
-// module.exports = mongoose
 require('dotenv').config()
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true}).then(() => {
-    console.log('Connected to mongo');
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+
+mongoose.connection.once('open', () => {
+    console.log(`Mongoose has connected to MongoDB`)
+})
+
+mongoose.connection.on('error', (error) => {
+    console.error(`MongoDB connection error!!! ${error}`)
+    process.exit(-1)
 })
 
 module.exports = mongoose
+// require('dotenv').config()
+// const mongoose = require('mongoose')
+
+// mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true}).then(() => {
+//     console.log('Connected to mongo');
+// })
+
+// module.exports = mongoose
 
