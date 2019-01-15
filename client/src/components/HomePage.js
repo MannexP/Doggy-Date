941bb5265bc550e859b135dd2c2ffa175df5fa4a
Mannex Pimentel <Mannex@mannexs-air.attlocal.net>
2019-01-14 22:23:13 -0500

    setting page

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -4,19 +4,35 @@ import mainTheme from '../images/dogsPlay.jpg';
 // import HomeCarousel from './HomeCarousel '
 
 const MainTheme = styled.div`
-  
-img {   
+  position: relative;
 
+  .text-block {
+  height: 30rem;
+  width: 70rem;
+  position: absolute;
+  bottom: 5rem;
+  right: 20px;
+  background-color: black;
+  opacity: 0.5;
+  border-radius: 2%;
+  color: white;
+  padding-left: 20px;
+  padding-right: 20rem;
 }
+h4{}
 `
 class HomePage extends Component {
     render() {
         return (
             <div>
                 <MainTheme>
-                    <img src={mainTheme} alt=""/>
+                    <img src={mainTheme} alt="" />
+                    <div className="text-block">
+                        <h4>Nature</h4>
+                        <p>What a beautiful sunrise</p>
+                    </div>
                 </MainTheme>
-               {/* <HomeCarousel></HomeCarousel> */}
+
             </div>
         );
     }
