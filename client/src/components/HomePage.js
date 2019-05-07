fe13e49a9cb8a72df7303dac3d657eb64b0965a5
Mannex Pimentel <mannex247@gmail.com>
2019-05-07 16:53:57 -0400

    splash styles

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,78 +1,107 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import styled from 'styled-components';
-import HeroTheme from '../images/Daug-splash.jpg';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import HeroTheme from "../images/Daug-splash.jpg";
 
+// const NavBarStyles = styled.div`
+//   display: flex;
+//   justify-content: space-between;
+//   align-items: center;
+//   background:blue;
+//   height: 8rem;
+//   h3{
+//     font-size: 4rem;
+//     font-weight:lighter
+//   }
+//   h5{
+//     font-size: 2rem;
+//     font-weight: lighter;
+//   }
 
-const NavBarStyles = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  background:blue;
-  height: 8rem;
-  h3{
-    font-size: 4rem;
-    font-weight:lighter
-  }
-  h5{
-    font-size: 2rem;
-    font-weight: lighter;
-  }
-  
-  a {
-    text-decoration: none;
-    padding-left: 10px;
-    color: white;
-    &:active {
-      color: red;
-    }
-  }
+//   a {
+//     text-decoration: none;
+//     padding-left: 10px;
+//     color: white;
+//     &:active {
+//       color: red;
+//     }
+//   }
 
-  .right {
-    width: 15vw;
-    display: flex;
-    justify-content: space-around;
-  }
-`
+//   .right {
+//     width: 15vw;
+//     display: flex;
+//     justify-content: space-around;
+//   }
+// `
 
 const HeroStyles = styled.div`
-background-color:black;
-height:100vh;
+*{
+    color:#ffffff;
+  }
+  background-color: black;
+  height: 100vh;
+  position: relative;
+  text-align: center;
+  color: white;
+  img {
+    width: 100vw;
+    height: 100vh;
+  }
+  .splash {
+    opacity: 0.6;
+  }
 
-img{
-  width:100vw;
-  height:100vh;
-  
- 
-}
-.splash{
-  opacity:0.9;
-}
+  .logo{
+    position: absolute;
+    top: 20px;
+    left: 100px;
+  }
 
-`
-
+  .nav-text {
+    position: absolute;
+    top: 20px;
+    right: 16px;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+  }
+  h5 {
+    color: #ffffff;
+    padding: 1rem 5rem;
+    font-weight: 300;
+  }
+`;
 
 class Hero extends Component {
   render() {
     return (
       <HeroStyles>
         <img class="splash" src={HeroTheme} alt="herotheme" />
-
-
-        {/* <NavBarStyles id="nav-container" className="some-class">
-        <Link to="/"><h3>Dog Date</h3></Link>
-        
-      
-        <div className="right">
-          <Link to="/login"><h5>Log In</h5></Link>
-          <Link to="/signup"><h5>Sign Ups</h5></Link>
-        </div>       
-      </NavBarStyles> */}
+        <Link class="logo" to="/">
+            <h3>Daüg Date</h3>
+          </Link>
+        <div className="nav-text">
+          <Link to="/login">
+            <h5>Log In</h5>
+          </Link>
+          <Link to="/signup">
+            <h5>Sign Up</h5>
+          </Link>
+        </div>
       </HeroStyles>
-      
-      
     );
   }
 }
 
 export default Hero;
+
+{
+  /* <NavBarStyles id="nav-container" className="some-class">
+      
+        
+      
+        <div className="right">
+        
+        </div>       
+      </NavBarStyles> */
+}
